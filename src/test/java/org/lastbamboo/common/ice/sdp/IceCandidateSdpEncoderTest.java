8cd2946042924145db87c7434864c714ab05eec7
adamfisk <a@littleshoot.org>
2007-06-29 19:43:27 +0000

    Updated test for new encoding
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/ice@700 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/test/java/org/lastbamboo/common/ice/sdp/IceCandidateSdpEncoderTest.java
+++ src/test/java/org/lastbamboo/common/ice/sdp/IceCandidateSdpEncoderTest.java
@@ -2,36 +2,36 @@ package org.lastbamboo.common.ice.sdp;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 import junit.framework.TestCase;
 
+import org.apache.commons.id.uuid.UUID;
 import org.apache.commons.lang.math.NumberUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.easymock.MockControl;
-import org.lastbamboo.common.ice.BindingTracker;
+import org.lastbamboo.common.ice.IceCandidate;
 import org.lastbamboo.common.ice.IceTransportProtocol;
+import org.lastbamboo.common.ice.TcpPassiveIceCandidate;
+import org.lastbamboo.common.ice.UdpIceCandidate;
 import org.lastbamboo.common.sdp.api.Attribute;
 import org.lastbamboo.common.sdp.api.MediaDescription;
-import org.lastbamboo.common.sdp.api.SdpFactory;
 import org.lastbamboo.common.sdp.api.SessionDescription;
-import org.lastbamboo.common.util.NetworkUtils;
 
 /**
  * Test for the class for generating SDP data.
  */
-public final class SdpFactoryImplTest extends TestCase
+public final class IceCandidateSdpEncoderTest extends TestCase
     {
 
     /**
      * Logger for this class.
      */
-    private static final Log LOG = LogFactory.getLog(SdpFactoryImplTest.class);
+    private static final Log LOG = LogFactory.getLog(IceCandidateSdpEncoderTest.class);
     
     /**
      * Tests the method for creating SDP for the local host.
@@ -39,36 +39,39 @@ public final class SdpFactoryImplTest extends TestCase
      */
     public void testCreateSdp() throws Exception
         {
-        final MockControl trackerControl = 
-            MockControl.createControl(BindingTracker.class);
-        final BindingTracker tracker = 
-            (BindingTracker) trackerControl.getMock();
-        
-        tracker.getTurnTcpBindings();
-        
-        final Collection localTcpBindings = createTcpLocalAddresses();
-        
-        final Collection tcpBindings = createTcpAddresses();
-        trackerControl.setReturnValue(tcpBindings, 1);
-        tracker.getStunUdpBinding();
-        final InetSocketAddress udpBinding = 
-            new InetSocketAddress(InetAddress.getByName("59.8.46.6"), 7888);
-        trackerControl.setReturnValue(udpBinding, 1);
-        
-        tracker.getTcpSoBinding();
-        trackerControl.setReturnValue(null);
-        
-        trackerControl.replay();
-        final IceCandidateAttributeFactory candidateAttributeFactory =
-            new IceCandidateAttributeFactoryImpl(new SdpFactory());
         
         final org.lastbamboo.common.sdp.api.SdpFactory sdpFactory = 
             org.lastbamboo.common.sdp.api.SdpFactory.getInstance();
         
-        final SdpFactoryImpl factory = 
-            new SdpFactoryImpl(tracker, candidateAttributeFactory, sdpFactory);
-        final SessionDescription sdp = factory.createSdp();
-        trackerControl.verify();
+        final IceCandidateSdpEncoder encoder = new IceCandidateSdpEncoder();
+        
+        final InetSocketAddress sa1 = 
+            new InetSocketAddress("46.2.62.1", 5466);
+        final InetSocketAddress sa2 = 
+            new InetSocketAddress("12.12.32.1", 4232);
+        final InetSocketAddress sa3 = 
+            new InetSocketAddress("192.168.1.3", 7652);
+        
+        final UdpIceCandidate udpCandidate = 
+            new UdpIceCandidate(1, UUID.randomUUID(), 3, sa1);
+        
+        final TcpPassiveIceCandidate tcpPassiveCandidate = 
+            new TcpPassiveIceCandidate(1, UUID.randomUUID(), 5, sa2);
+        
+        final TcpPassiveIceCandidate localCandidate = 
+            new TcpPassiveIceCandidate(1, UUID.randomUUID(), 5, sa3);
+        
+        final Collection<IceCandidate> candidates = 
+            new LinkedList<IceCandidate>();
+        
+        candidates.add(udpCandidate);
+        candidates.add(tcpPassiveCandidate);
+        candidates.add(localCandidate);
+        encoder.visitCandidates(candidates);
+        final byte[] sdpBytes = encoder.getSdp();
+        
+        final SessionDescription sdp = 
+            sdpFactory.createSessionDescription(new String(sdpBytes));
         
         final Collection mediaDescriptions = sdp.getMediaDescriptions(true);
         
@@ -85,7 +88,13 @@ public final class SdpFactoryImplTest extends TestCase
         // Just create a collection with one element for the UDP test.
         final Collection<InetSocketAddress> udpBindings = 
             new HashSet<InetSocketAddress>();
-        udpBindings.add(udpBinding);
+        udpBindings.add(sa1);
+        final Collection<InetSocketAddress> tcpBindings = 
+            new HashSet<InetSocketAddress>();
+        tcpBindings.add(sa2);
+        final Collection<InetSocketAddress> localTcpBindings = 
+            new HashSet<InetSocketAddress>();
+        localTcpBindings.add(sa3);
         verifyCandidates(udpMediaDesc, udpBindings, "udp");
         verifyCandidates(tcpMediaDesc, tcpBindings, 
             IceTransportProtocol.TCP_PASS.getName());
@@ -138,23 +147,5 @@ public final class SdpFactoryImplTest extends TestCase
             }
         
         assertEquals(bindings.size(), numCandidates);
-        }
-
-    private Collection createTcpLocalAddresses() throws UnknownHostException
-        {
-        final InetSocketAddress socketAddress = 
-            new InetSocketAddress(NetworkUtils.getLocalHost(), 8107);
-        final Collection addresses = new HashSet();
-        addresses.add(socketAddress);      
-        return addresses;
-        }
-    
-    private Collection createTcpAddresses() throws UnknownHostException
-        {
-        final InetSocketAddress socketAddress = 
-            new InetSocketAddress(InetAddress.getByName("39.5.46.6"), 7888);
-        final Collection addresses = new HashSet();
-        addresses.add(socketAddress);      
-        return addresses;
         }
     }
