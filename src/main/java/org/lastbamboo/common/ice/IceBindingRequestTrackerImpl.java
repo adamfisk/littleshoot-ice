0c87fcb6610d65449a101db0d859faeb041811f1
adamfisk <a@littleshoot.org>
2007-11-14 15:58:06 +0000

    Improved data structure usage
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/ice@1560 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/ice/IceBindingRequestTrackerImpl.java
+++ src/main/java/org/lastbamboo/common/ice/IceBindingRequestTrackerImpl.java
@@ -1,33 +1,34 @@
 package org.lastbamboo.common.ice;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Collections;
+import java.util.LinkedHashSet;
 
 import org.apache.commons.id.uuid.UUID;
 import org.lastbamboo.common.stun.stack.message.BindingRequest;
 
+/**
+ * Keeps track of Binding Requests we've seen to avoid processing a request
+ * for the same transaction twice.
+ */
 public class IceBindingRequestTrackerImpl implements IceBindingRequestTracker
     {
     
-    private final Collection<UUID> m_transactionIds = new HashSet<UUID>();
-    private final Queue<UUID> m_fifoTransactionIds =
-        new ConcurrentLinkedQueue<UUID>();
+    private final Collection<UUID> m_transactionIds = 
+        Collections.synchronizedSet(new LinkedHashSet<UUID>());
     
-    private static final int UUIDS_TO_STORE = 100;
+    private static final int UUIDS_TO_STORE = 200;
     
     public void add(final BindingRequest request)
         {
-        synchronized (this)
+        synchronized (m_transactionIds)
             {
             if (this.m_transactionIds.size() >= UUIDS_TO_STORE)
                 {
-                final UUID lastIn = this.m_fifoTransactionIds.poll();
+                final UUID lastIn = this.m_transactionIds.iterator().next();
                 this.m_transactionIds.remove(lastIn);
                 }
             this.m_transactionIds.add(request.getTransactionId());
-            this.m_fifoTransactionIds.add(request.getTransactionId());
             }
         }
 
