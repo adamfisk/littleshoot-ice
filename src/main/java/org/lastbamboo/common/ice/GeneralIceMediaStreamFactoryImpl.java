13111ad6cf4212240194dad97609c085ed08215f
adamfisk <a@littleshoot.org>
2008-06-29 23:28:52 +0000

    fixed protocol ordering
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/ice@3310 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/ice/GeneralIceMediaStreamFactoryImpl.java
+++ src/main/java/org/lastbamboo/common/ice/GeneralIceMediaStreamFactoryImpl.java
@@ -52,8 +52,8 @@ public class GeneralIceMediaStreamFactoryImpl
         final DemuxableProtocolCodecFactory stunCodecFactory =
             new StunDemuxableProtocolCodecFactory();
         final ProtocolCodecFactory demuxingCodecFactory = 
-            new DemuxingProtocolCodecFactory(protocolCodecFactory,
-                stunCodecFactory);
+            new DemuxingProtocolCodecFactory(
+                stunCodecFactory, protocolCodecFactory);
         final StunTransactionTracker<StunMessage> transactionTracker =
             new StunTransactionTrackerImpl();
 
