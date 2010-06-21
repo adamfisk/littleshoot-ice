018d301fe7ced1d51938a0d5077e545c2af40d93
adamfisk <a@littleshoot.org>
2010-06-21 22:59:09 +0000

    Changed to remove filters on close
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/ice@7127 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/ice/transport/IceUdpConnector.java
+++ src/main/java/org/lastbamboo/common/ice/transport/IceUdpConnector.java
@@ -1,12 +1,15 @@
 package org.lastbamboo.common.ice.transport;
 
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.util.Collection;
 import java.util.LinkedList;
 
 import org.littleshoot.mina.common.ConnectFuture;
 import org.littleshoot.mina.common.ExecutorThreadModel;
 import org.littleshoot.mina.common.IoHandler;
+import org.littleshoot.mina.common.IoService;
+import org.littleshoot.mina.common.IoServiceConfig;
 import org.littleshoot.mina.common.IoServiceListener;
 import org.littleshoot.mina.common.IoSession;
 import org.littleshoot.mina.common.RuntimeIOException;
@@ -22,7 +25,7 @@ import org.slf4j.LoggerFactory;
  * Class for creating a UDP "connection" for ICE.  This really just sets up
  * the UDP transport for a UDP connectivity check. 
  */
-public class IceUdpConnector implements IceConnector
+public class IceUdpConnector implements IceConnector, IoServiceListener
     {
     
     private final Logger m_log = LoggerFactory.getLogger(getClass());
@@ -32,6 +35,7 @@ public class IceUdpConnector implements IceConnector
     
     private final Collection<IoServiceListener> m_serviceListeners =
         new LinkedList<IoServiceListener>();
+    private final DatagramConnector m_connector;
 
     /**
      * Creates a new UDP connector.
@@ -49,20 +53,21 @@ public class IceUdpConnector implements IceConnector
         m_demuxingCodecFactory = protocolCodecFactory;
         m_demuxingIoHandler = demuxingIoHandler;
         m_controlling = controlling;
+        this.m_connector = new DatagramConnector();
+        this.m_connector.addListener(this);
         }
 
     public IoSession connect(final InetSocketAddress localAddress,
         final InetSocketAddress remoteAddress)
         {
-        final DatagramConnector connector = new DatagramConnector();
         synchronized (this.m_serviceListeners)
             {
             for (final IoServiceListener listener : this.m_serviceListeners)
                 {
-                connector.addListener(listener);
+                this.m_connector.addListener(listener);
                 }
             }
-        final DatagramConnectorConfig cfg = connector.getDefaultConfig();
+        final DatagramConnectorConfig cfg = this.m_connector.getDefaultConfig();
         cfg.getSessionConfig().setReuseAddress(true);
         
         final ThreadModel threadModel = ExecutorThreadModel.getInstance(
@@ -73,13 +78,13 @@ public class IceUdpConnector implements IceConnector
             new ProtocolCodecFilter(this.m_demuxingCodecFactory);
         cfg.setThreadModel(threadModel);
         
-        connector.getFilterChain().addLast("demuxFilter", demuxingFilter);
+        this.m_connector.getFilterChain().addLast("demuxFilter", demuxingFilter);
 
         m_log.debug("Connecting from "+localAddress+" to "+
             remoteAddress);
         
         final ConnectFuture cf = 
-            connector.connect(remoteAddress, localAddress, 
+            this.m_connector.connect(remoteAddress, localAddress, 
                 this.m_demuxingIoHandler);
         
         cf.join();
@@ -112,4 +117,31 @@ public class IceUdpConnector implements IceConnector
         {
         this.m_serviceListeners.add(serviceListener);
         }
+
+    public void serviceActivated(IoService arg0, SocketAddress arg1,
+            IoHandler arg2, IoServiceConfig arg3) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void serviceDeactivated(IoService arg0, SocketAddress arg1,
+            IoHandler arg2, IoServiceConfig arg3) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void sessionCreated(IoSession arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void sessionDestroyed(final IoSession session) {
+        m_log.info("Got session closed: {}", session);
+        try {
+            this.m_connector.getFilterChain().clear();
+        } catch (final Exception e) {
+            m_log.warn("Exception clearing filter chaing!!",e);
+        }
+        //this.m_connector.getFilterChain().clear();
+    }
     }
