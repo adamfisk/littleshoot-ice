af059b34beb2d7d437fd2a40012e0fbe4012a167
adamfisk <a@littleshoot.org>
2010-12-05 20:56:56 +0000

    port mapping cleanups
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/ice@7607 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/ice/MappedTcpAnswererServer.java
+++ src/main/java/org/lastbamboo/common/ice/MappedTcpAnswererServer.java
@@ -6,8 +6,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import org.lastbamboo.common.portmapping.NatPmpService;
-import org.lastbamboo.common.portmapping.PortMapErrorEvent;
-import org.lastbamboo.common.portmapping.PortMapEvent;
 import org.lastbamboo.common.portmapping.PortMapListener;
 import org.lastbamboo.common.portmapping.PortMappingProtocol;
 import org.lastbamboo.common.portmapping.UpnpService;
@@ -23,18 +21,11 @@ import org.slf4j.LoggerFactory;
  * data to the HTTP server. We can't do the same on the offerer/client side
  * because we have to map incoming sockets to the particular ICE session.
  */
-public class MappedTcpAnswererServer implements PortMapListener 
-    {
+public class MappedTcpAnswererServer implements PortMapListener {
 
     private final Logger m_log = LoggerFactory.getLogger(getClass());
     
-    private final NatPmpService m_natPmpService;
-    private final UpnpService m_upnpService;
     private ServerSocket m_serverSocket;
-
-    protected int m_upnpMappingIndex;
-
-    protected int m_natPmpMappingIndex;
 
     /**
      * Creates a new mapped server for the answerer.
@@ -44,90 +35,59 @@ public class MappedTcpAnswererServer implements PortMapListener
      * @param socketListener Class for handling new incoming sockets.
      * @throws IOException If there's an error starting the server.
      */
-    public MappedTcpAnswererServer(final NatPmpService natPmpService, 
-        final UpnpService upnpService, final SocketListener socketListener) 
-        throws IOException
-        {
-        this.m_natPmpService = natPmpService;
-        this.m_upnpService = upnpService;
-        final Runnable serverRunner = new Runnable() 
-            {
-            public void run()
-                {
-                try 
-                    {
+    public MappedTcpAnswererServer(final NatPmpService natPmpService,
+        final UpnpService upnpService, final SocketListener socketListener)
+        throws IOException {
+        final Runnable serverRunner = new Runnable() {
+            public void run() {
+                try {
                     establishSocket();
-                    }
-                catch (final IOException e) 
-                    {
+                } catch (final IOException e) {
                     m_log.warn("Could not create server socket or map port?",e);
                     return;
-                    }
-                
-                // We just accept the single socket on this port instead of
-                while (true)
-                    {
-                    try
-                        {
+                }
+                while (true) {
+                    try {
                         final Socket sock = m_serverSocket.accept();
                         sock.setSoTimeout(100 * 60 * 1000);
                         m_log.info("ACCEPTED INCOMING SOCKET!!");
                         socketListener.onSocket(sock);
-                        }
-                    catch (final IOException e)
-                        {
+                    } catch (final IOException e) {
                         m_log.info("Exception accepting socket!!", e);
-                        }
                     }
                 }
+            }
 
-            private void establishSocket() throws IOException
-                {
+            private void establishSocket() throws IOException {
                 upnpService.addPortMapListener(MappedTcpAnswererServer.this);
                 natPmpService.addPortMapListener(MappedTcpAnswererServer.this);
 
                 m_serverSocket = new ServerSocket();
                 m_serverSocket.bind(
                     new InetSocketAddress(NetworkUtils.getLocalHost(), 0));
-                final InetSocketAddress socketAddress =
+                final InetSocketAddress socketAddress = 
                     (InetSocketAddress) m_serverSocket.getLocalSocketAddress();
                 final int port = socketAddress.getPort();
-                m_upnpMappingIndex = 
-                    upnpService.addUpnpMapping(PortMappingProtocol.TCP, port, port);
-                m_natPmpMappingIndex =
-                    natPmpService.addNatPmpMapping(PortMappingProtocol.TCP, port, port);
-                }
-            };
-        final Thread serverThread = new Thread(serverRunner, 
-            "TCP-Ice-Server-Answerer-Thread-"+hashCode());
+                upnpService.addUpnpMapping(PortMappingProtocol.TCP, port, port);
+                natPmpService.addNatPmpMapping(PortMappingProtocol.TCP, port,
+                    port);
+            }
+        };
+        final Thread serverThread = new Thread(serverRunner,
+                "TCP-Ice-Server-Answerer-Thread-" + hashCode());
         serverThread.setDaemon(true);
         serverThread.start();
-        
-        // We just want to make sure we release our UPnP ports as soon as the
-        // JVM exits.
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable()
-            {
-            public void run() 
-                {
-                m_upnpService.removeUpnpMapping(m_upnpMappingIndex);
-                m_natPmpService.removeNatPmpMapping(m_natPmpMappingIndex);
-                }
-            },
-            serverThread.getName() + "-ReleaseMappingsOnShutdown"));
-        }
-    
-    public InetSocketAddress getHostAddress() 
-        {
-        return (InetSocketAddress) this.m_serverSocket.getLocalSocketAddress();
-        }
-        
-    public void onPortMap(final PortMapEvent portMapEvent) 
-        {
-        m_log.info("Received port map event: {}", portMapEvent);
-        }
-
-    public void onPortMapError(final PortMapErrorEvent portMapErrorEvent) 
-        {
-        m_log.info("Got port map error: {}", portMapErrorEvent);
-        }
     }
+    
+    public InetSocketAddress getHostAddress() {
+        return (InetSocketAddress) this.m_serverSocket.getLocalSocketAddress();
+    }
+
+    public void onPortMap(final int externalPort) {
+        m_log.info("Received port maped: {}", externalPort);
+    }
+
+    public void onPortMapError() {
+        m_log.info("Got port map error.");
+    }
+}
